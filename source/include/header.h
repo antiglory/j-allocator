980c70cefbc11b0e8e80632cea5f58f286486f9a
dezska <104264903+antiglory@users.noreply.github.com>
2024-06-15 21:30:14 -0300

    Add files via upload

---
+++ source/include/header.h
@@ -0,0 +1,41 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+// system libs including
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+
+// macros
+#define TRUE 1
+#define FALSE 0
+
+#define CHUNK_ALIGNMENT 8
+
+#define NUM_BINS 8
+#define BIN_SIZE_INCREMENT 128
+
+#define PROT_READ_BIT   0x1
+#define PROT_WRITE_BIT  0x2
+#define PROT_EXEC_BIT   0x4
+
+// typedefs
+typedef unsigned char byte_t;
+
+typedef struct chunk_t {
+    size_t size;
+    byte_t priv;
+
+    byte_t CHUNK_INUSE;
+    byte_t PREV_INUSE;
+    byte_t NON_MAIN_ARENA;
+    byte_t IS_MMAPED;
+
+    struct chunk_t* fd;
+    struct chunk_t* bk;
+} chunk_t;
+
+#endif
---
+++ source/main.c
@@ -0,0 +1,107 @@
+#include "include/header.h"
+
+chunk_t* cachebin[NUM_BINS] = {NULL};
+
+// just-align-size
+size_t jalignsize(size_t size) {
+    if (size % CHUNK_ALIGNMENT != 0) {
+        size += CHUNK_ALIGNMENT - (size % CHUNK_ALIGNMENT);
+    }
+    return size;
+}
+
+// just-get-bin-index
+int jgetbinindex(const size_t size) {
+    return (size / BIN_SIZE_INCREMENT) < NUM_BINS ? (size / BIN_SIZE_INCREMENT) : (NUM_BINS - 1);
+}
+
+// just-allocate
+void* jalloc(const size_t _Size, const byte_t _Priv) {
+    int prot, flags;
+    void* page_start;
+
+    if ((_Priv & 0x7) != _Priv) {
+        return NULL;
+    }
+
+    if (_Priv & PROT_EXEC_BIT) {
+        prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+    } else {
+        prot = PROT_READ | PROT_WRITE;
+    }
+
+    flags = MAP_PRIVATE | MAP_ANONYMOUS;
+
+    const size_t f_Size = jalignsize(_Size + sizeof(chunk_t));
+
+    // find the appropriate bin
+    const int binindex = jgetbinindex(f_Size);
+    chunk_t* current = cachebin[binindex];
+    chunk_t* prev = NULL;
+
+    // search for a reusable chunk in the current bin
+    while (current != NULL) {
+        if (current->size >= f_Size && current->CHUNK_INUSE == FALSE) {
+            current->CHUNK_INUSE = TRUE;
+            current->priv = _Priv;
+            void* payload_area = (void*)((char*)current + sizeof(chunk_t));
+            return payload_area;
+        }
+        prev = current;
+        current = current->fd;
+    }
+
+    // no reusable chunk found then allocate one
+    chunk_t* lastchunk = NULL;
+    if (prev != NULL) {
+        lastchunk = prev;
+        page_start = (void*)((char*)lastchunk + lastchunk->size + CHUNK_ALIGNMENT);
+    } else {
+        page_start = mmap(NULL, f_Size, prot, flags, -1, 0);
+        if (page_start == MAP_FAILED) return NULL;
+    }
+
+    memset(page_start, 0, sizeof(chunk_t));
+
+    chunk_t new_chunk;
+    new_chunk.size = f_Size;
+    new_chunk.priv = _Priv;
+    new_chunk.fd = NULL;
+    new_chunk.CHUNK_INUSE = TRUE;
+    new_chunk.IS_MMAPED = TRUE;
+    new_chunk.PREV_INUSE = FALSE;
+    new_chunk.NON_MAIN_ARENA = FALSE;
+
+    memcpy(page_start, &new_chunk, sizeof(chunk_t));
+
+    if (prev != NULL) {
+        prev->fd = page_start;
+    } else {
+        cachebin[binindex] = page_start;
+    }
+
+    void* payload_area = (void*)((char*)page_start + sizeof(chunk_t));
+
+    return payload_area;
+}
+
+// just-free
+void jfree(void* ptr) {
+    if (ptr == NULL) return;
+
+    chunk_t* chunk = (chunk_t*)((char*)ptr - sizeof(chunk_t));
+    chunk->CHUNK_INUSE = FALSE;
+}
+
+int main(void) {
+    int* heap = jalloc(2 * sizeof(int), PROT_READ_BIT | PROT_WRITE_BIT);
+    if (!heap) puts("deu ruim");
+    *heap = 10;
+
+    printf("[%p]   : %d\n", heap, *heap);
+
+    jfree(heap);
+    return 0;
+}
+
+// meu codigo nao foi feito pra ser legivel/compreendivel

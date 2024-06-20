1cc8522e2b3251b1a1d8f25ceb66ba983da86ec6
dezska <104264903+antiglory@users.noreply.github.com>
2024-06-19 21:32:33 -0300

    Update header.h

--- source/include/header.h
+++ source/include/header.h
@@ -13,27 +13,26 @@
 #define TRUE 1
 #define FALSE 0
 
-#define CHUNK_ALIGNMENT 8
+#define CHUNK_ALIGNMENT_BYTES 8
 
-#define NUM_BINS 8
-#define BIN_SIZE_INCREMENT 128
+#define JCACHE_BINS_NUM 8
+#define JCACHE_BIN_SIZE_INCREMENT 128
 
 #define PROT_READ_BIT   0x1
 #define PROT_WRITE_BIT  0x2
 #define PROT_EXEC_BIT   0x4
+
+#define CHUNK_INUSE_BIT     0x1
+#define PREV_INUSE_BIT      0x2
+#define NON_MAIN_ARENA_BIT  0x4
+#define IS_MMAPED_BIT       0x8
 
 // typedefs
 typedef unsigned char byte_t;
 
 typedef struct chunk_t {
     size_t size;
-    byte_t priv;
-
-    byte_t CHUNK_INUSE;
-    byte_t PREV_INUSE;
-    byte_t NON_MAIN_ARENA;
-    byte_t IS_MMAPED;
-
+    byte_t flags;
     struct chunk_t* fd;
     struct chunk_t* bk;
 } chunk_t;

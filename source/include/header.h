5ddb6994dcafc2caafaf0c377a1dc60a651415e1
dezska <104264903+antiglory@users.noreply.github.com>
2024-06-19 21:35:49 -0300

    Update header.h

--- source/include/header.h
+++ source/include/header.h
@@ -22,19 +22,18 @@
 #define PROT_WRITE_BIT  0x2
 #define PROT_EXEC_BIT   0x4
 
-#define CHUNK_INUSE_BIT     0x1
+#define INUSE_BIT           0x1
 #define PREV_INUSE_BIT      0x2
-#define NON_MAIN_ARENA_BIT  0x4
-#define IS_MMAPED_BIT       0x8
+#define IS_MMAPED_BIT       0x4
 
 // typedefs
 typedef unsigned char byte_t;
 
 typedef struct chunk_t {
     size_t size;
-    byte_t flags;
-    struct chunk_t* fd;
-    struct chunk_t* bk;
+    byte_t flags; // PREV_INUSE, INUSE and MMAPED
+    struct chunk_t* fd; // forward chunk pointer
+    struct chunk_t* bk; // backward chunk pointer
 } chunk_t;
 
 #endif

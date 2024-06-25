56b353e96ba4cf73b9abf7600e1d05efb37e601a
dezska <104264903+antiglory@users.noreply.github.com>
2024-06-25 17:31:17 -0300

    Update main.h

--- source/include/main.h
+++ source/include/main.h
@@ -11,7 +11,10 @@
 #define TRUE  1
 #define FALSE 0
 
-#define CHUNK_ALIGNMENT_BYTES 8
+#define JC_ERROR_INVALID_POINTER 0x22
+
+#define JF_ERROR_DOUBLE_FREE      0x69
+#define JF_ERROR_INVALID_POINTER  0x70
 
 #define JCACHE_CHUNK_AMOUNT   16
 #define JCACHE_SIZE_INCREMENT 128
@@ -24,6 +27,8 @@
 #define PREV_INUSE_BIT  0x2
 #define IS_MMAPED_BIT   0x4
 
+#define CHUNK_ALIGNMENT_BYTES 8
+
 // typedefs
 typedef unsigned char byte_t;
 
@@ -33,6 +38,7 @@ typedef struct chunk_t {
     byte_t flags;       // PREV_INUSE, INUSE and MMAPED
     struct chunk_t* fd; // forward chunk pointer
     struct chunk_t* bk; // backward chunk pointer
+    // chunk_t is approximately 40 bytes long
 } chunk_t;
 
 #endif
